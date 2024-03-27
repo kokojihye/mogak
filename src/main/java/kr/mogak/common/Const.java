c2944728c4b05c1c75a4a54d6e9d24831f492892
kokojihye <jihyevivianako@gmail.com>
2024-03-27 10:26:40 +0900

    feat: add message

---
+++ src/main/java/kr/mogak/common/Const.java
@@ -0,0 +1,20 @@
+package kr.mogak.common;
+
+public class Const {
+    // COMMON_MESSAGE
+    // BOARD_MESSAGE
+    public static final String BOARD_POST_REGIST_SUCCESS = "게시글이 등록되었습니다.";
+    public static final String BOARD_POST_REGIST_FAIL = "게시글 등록 중 오류가 발생하였습니다.";
+    public static final String BOARD_POST_REPORT_SUCCESS = "게시글이 신고 되었습니다.";
+    public static final String BOARD_POST_REPORT_FAIL = "게시글 신고 중 오류가 발생하였습니다.";
+
+    // COMMENT_MESSAGE
+    public static final String COMMENT_REGIST_SUCCESS = "댓글이 등록되었습니다.";
+    public static final String COMMENT_REGIST_FAIL = "댓글 등록 중 오류가 발생하였습니다.";
+
+    // ADMIN_MESSAGE
+    public static final String ADMIN_BLIND_POST_SUCCESS = "게시글 가리기 처리가 완료되었습니다.";
+    public static final String ADMIN_BLIND_POST_FAIL = "게시글 가리기 작업 중 오류가 발생하였습니다..";
+    public static final String ADMIN_BLIND_COMMENT_SUCCESS = "댓글 가리기 처리가 완료되었습니다.";
+    public static final String ADMIN_BLIND_COMMENT_FAIL = "댓글 가리기 작업 중 오류가 발생하였습니다.";
+}
