a861010d80c8625900d618f75f7fca9fa8c265a2
kokojihye <jihyevivianako@gmail.com>
2024-04-17 20:03:56 +0900

    add: 로그인 인터셉터 코드 변경

--- src/main/java/kr/mogak/common/LoginCheckInterceptor.java
+++ src/main/java/kr/mogak/common/LoginCheckInterceptor.java
@@ -1,4 +1,25 @@
 package kr.mogak.common;
 
-public class CustomInterceptor {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+@Slf4j
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+
+        // 기존 session이 있으면 해당 session을, 없으면 null 반환
+        HttpSession session = request.getSession(false);
+
+        if(session == null || session.getAttribute("loginMember") == null) {
+            log.info("미인증 사용자 요청");
+            // TODO login으로 redirect
+            // response.sendRedirect();
+            return false;
+        }
+        return true;
+    }
 }
--- src/main/java/kr/mogak/common/WebConfig.java
+++ src/main/java/kr/mogak/common/WebConfig.java
@@ -1,2 +1,16 @@
-package kr.mogak.common;public class WebConfig {
+package kr.mogak.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns() // 적용할 경로
+                .excludePathPatterns(); // 제외할 경로
+    }
 }
