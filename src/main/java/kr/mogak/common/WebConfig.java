447c4d0586f144828e0a06b33e17575155e53a2d
kokojihye <jihyevivianako@gmail.com>
2024-04-18 19:30:48 +0900

    add: 권한 체크 인터셉터 추가

--- src/main/java/kr/mogak/common/WebConfig.java
+++ src/main/java/kr/mogak/common/WebConfig.java
@@ -1,16 +1,24 @@
 package kr.mogak.common;
 
+import kr.mogak.common.interceptor.AuthCheckInterceptor;
+import kr.mogak.common.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(1)
                 .addPathPatterns() // 적용할 경로
                 .excludePathPatterns(); // 제외할 경로
     }
+
+    public void addAuthInterceptor(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthCheckInterceptor())
+                .addPathPatterns()
+                .excludePathPatterns();
+    }
+
 }
