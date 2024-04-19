7d621e90e97891c50f26d1cd8ed9c1f73d07cc03
kokojihye <jihyevivianako@gmail.com>
2024-04-19 19:39:20 +0900

    feat: post 목록 조회 작업중

--- src/main/java/kr/mogak/service/PostService.java
+++ src/main/java/kr/mogak/service/PostService.java
@@ -1,12 +1,27 @@
 package kr.mogak.service;
 
+import kr.mogak.dto.response.PostResponse;
+import kr.mogak.entity.Post;
+import kr.mogak.enums.Yn;
+import kr.mogak.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class PostService {
+    private PostRepository postRepository;
+    public List<PostResponse> getPostList() {
+        List<Post> posts = postRepository.findByDeletedYn(Yn.Y);
+        List<PostResponse> postResponses = new ArrayList<>();
 
+        // TODO posts postresponses에 매핑
+        return postResponses;
+    }
 }
